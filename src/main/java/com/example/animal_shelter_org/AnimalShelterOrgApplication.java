2b4ac1dbcdc443d4e4facb098fac531c34400c18
DidierSML <113539961+DidierSML@users.noreply.github.com>
2023-03-07 16:51:36 -0500

    Initial commit

---
+++ src/main/java/com/example/animal_shelter_org/AnimalShelterOrgApplication.java
@@ -0,0 +1,13 @@
+package com.example.animal_shelter_org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalShelterOrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/animal_shelter_org/AnimalShelterOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animal_shelter_org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
