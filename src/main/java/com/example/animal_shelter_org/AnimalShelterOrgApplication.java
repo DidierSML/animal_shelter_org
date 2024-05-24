037f30e4e9ac860c359be0a0c4afd602a0f86c1f
DidierSML <didiersmaldonad@gmail.com>
2024-05-24 09:11:25 -0500

    Updating pom.xml Generating API Documentation through -Swagger- AND adding Plugging maven-surefire-plugin to avoid jar.file Error

--- src/main/java/com/example/animal_shelter_org/AnimalShelterOrgApplication.java
+++ src/main/java/com/example/animal_shelter_org/AnimalShelterOrgApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableJpaRepositories("com.example.animal_shelter_org.repository")
-//@EntityScan("com.example.animal_shelter_org.model")
 public class AnimalShelterOrgApplication {
 
 	public static void main(String[] args) {
